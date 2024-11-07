a90655b3608c152b10679d9f9e3832742d928105
ign-is <igna.dev.contact@gmail.com>
2024-11-07 17:56:24 -0300

    Add 20-files

---
+++ 20-files/index.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+// Open file
+$file = fopen("text_file.txt", "a+");
+
+// Read content
+while(!feof($file)) {
+    $content = fgets($file);
+    echo $content."<br/>";
+}
+
+// Write
+fwrite($file, "I'm text entered from PHP");
+
+// Close file
+fclose($file);
+*/
+
+// Copy file
+//copy('text_file.txt', 'copied_file.txt') or die('Error copying');
+
+// Rename
+//rename('text_file.txt', 'text_file_with_other_name.txt');
+
+// Delete
+//unlink('text_file_with_other_name.txt') or die('Error deleting');
+
+// if(file_exists('copied_file.txt')) {
+//     echo "This file exist!";
+// } else {
+//     echo "It doesn't exist!";
+// }
+
+
+?>
