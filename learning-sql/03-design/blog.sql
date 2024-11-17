5ea78adaedb646c6eca89d836b330404a148578a
ign-is <igna.dev.contact@gmail.com>
2024-11-17 19:46:40 -0300

    Add entradas table

--- learning-sql/03-design/blog.sql
+++ learning-sql/03-design/blog.sql
@@ -15,3 +15,14 @@ nombre  varchar(100),
 CONSTRAINT pk_categorias PRIMARY KEY(id)
 )ENGINE=InnoDb;
 
+CREATE TABLE entradas(
+id              int(255) auto_increment not null,
+usuario_id      int(255) not null,
+categoria_id    int(255) not null,
+titulo          varchar(255) not null,
+descripcion     MEDIUMTEXT,
+fecha           date not null,
+CONSTRAINT pk_entradas PRIMARY KEY(id),
+CONSTRAINT fk_entrada_usuario FOREIGN KEY(usuario_id) REFERENCES usuarios(id),
+CONSTRAINT fk_entrada_categoria FOREIGN KEY(categoria_id) REFERENCES categorias(id) ON DELETE NO ACTION
+)ENGINE=InnoDb;
