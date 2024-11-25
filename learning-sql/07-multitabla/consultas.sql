0b94b97cf508cbfd27f1d6f7f9d67677df871e9c
ign-is <igna.dev.contact@gmail.com>
2024-11-25 15:21:14 -0300

    Add inner join to consultas.sql

--- learning-sql/07-multitabla/consultas.sql
+++ learning-sql/07-multitabla/consultas.sql
@@ -8,3 +8,9 @@ Son consultas que sirven para consultar varias tablas en una sola sentencia
 SELECT e.id, e.titulo, u.nombre AS 'Autor', c.nombre AS 'Categoría'
 FROM entradas e, usuarios u, categorias c
 WHERE e.usuario_id = u.id AND e.categoria_id = c.id;
+
+#INNNER JOIN#
+SELECT e.id, e.titulo, u.nombre AS 'Autor', c.nombre AS 'Categoría'
+FROM entradas e
+INNER JOIN usuarios u ON e.usuario_id = u.id
+INNER JOIN categorias c ON e.categoria_id = c.id;
