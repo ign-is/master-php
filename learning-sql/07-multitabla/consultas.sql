82228d03b27b17f84060fc7e054c93eb70ee52f5
ign-is <igna.dev.contact@gmail.com>
2024-11-26 20:36:26 -0300

    Add update consultas.sql

--- learning-sql/07-multitabla/consultas.sql
+++ learning-sql/07-multitabla/consultas.sql
@@ -14,3 +14,11 @@ SELECT e.id, e.titulo, u.nombre AS 'Autor', c.nombre AS 'Categoría'
 FROM entradas e
 INNER JOIN usuarios u ON e.usuario_id = u.id
 INNER JOIN categorias c ON e.categoria_id = c.id;
+
+#Mostrar el nombre de las categorias y al lado cuantas entradas tienen#
+SELECT c.nombre, COUNT(e.id) FROM categorias c, entradas e
+WHERE e.categoria_id = c.id GROUP BY e.categoria_id;
+
+SELECT c.nombre, COUNT(e.id) FROM categorias c
+LEFT JOIN entradas e ON e.categoria_id = c.id
+GROUP BY e.categoria_id;
