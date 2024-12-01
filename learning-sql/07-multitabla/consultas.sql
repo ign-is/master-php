cc5aa806e7b200238a3e32e61f5c9cb6e8c00065
ign-is <igna.dev.contact@gmail.com>
2024-11-30 22:02:48 -0300

    Add consultas.sql finish update

--- learning-sql/07-multitabla/consultas.sql
+++ learning-sql/07-multitabla/consultas.sql
@@ -30,3 +30,7 @@ GROUP BY e.categoria_id;
 SELECT c.nombre, COUNT(e.id) FROM entradas e
 OUTER JOIN categorias c ON e.categoria_id = c.id
 GROUP BY e.categoria_id;
+
+#Mostrar el email de los usuarios y al lado cuantas entradas tienen#
+SELECT u.email, COUNT(titulo) FROM usuarios u, entradas e
+WHERE e.usuario_id = u.id GROUP BY e.usuario_id;
