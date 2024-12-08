edaa07af7d6f8df9327dfcec5c3b6196c2fc0333
ign-is <igna.dev.contact@gmail.com>
2024-12-07 21:08:04 -0300

    Add insert data to clientes and encargos

--- learning-sql/09-ejercicios/ejercicio01.sql
+++ learning-sql/09-ejercicios/ejercicio01.sql
@@ -89,3 +89,19 @@ INSERT INTO vendedores VALUES(NULL, 4, NULL, 'Antonio', 'Lopez', 'Vendedor de re
 INSERT INTO vendedores VALUES(NULL, 5, NULL, 'Salvador', 'Lopez', 'Vendedor experto', CURDATE(), 60000, 2);
 INSERT INTO vendedores VALUES(NULL, 6, NULL, 'Joaquin', 'Lopez', 'Ejecutivo de cuentas', CURDATE(), 80000, 1);
 INSERT INTO vendedores VALUES(NULL, 6, 8, 'Luis', 'Lopez', 'Ayudante en tienda', CURDATE(), 10000, 10);
+
+#CLIENTES
+INSERT INTO clientes VALUES(NULL, 1, 'Construcciones Diaz Inc', 'Alcobendas', 24000, CURDATE());
+INSERT INTO clientes VALUES(NULL, 1, 'Fruteria Antonia Inc', 'Fuenlabrada', 40000, CURDATE());
+INSERT INTO clientes VALUES(NULL, 1, 'Imprenta martinez Inc', 'Barcelona', 32000, CURDATE());
+INSERT INTO clientes VALUES(NULL, 1, 'Jesus Colchones Inc', 'El Prat', 96000, CURDATE());
+INSERT INTO clientes VALUES(NULL, 1, 'Bar Pepe Inc', 'Valencia', 170000, CURDATE());
+INSERT INTO clientes VALUES(NULL, 1, 'Tienda PC Inc', 'Murcia', 245000, CURDATE());
+
+#ENCARGOS
+INSERT INTO encargos VALUES(NULL, 1, 1, 2, CURDATE());
+INSERT INTO encargos VALUES(NULL, 2, 2, 4, CURDATE());
+INSERT INTO encargos VALUES(NULL, 3, 3, 1, CURDATE());
+INSERT INTO encargos VALUES(NULL, 4, 3, 3, CURDATE());
+INSERT INTO encargos VALUES(NULL, 5, 5, 1, CURDATE());
+INSERT INTO encargos VALUES(NULL, 6, 6, 1, CURDATE());
